f3680dd66a9e5fce1d8f1acd9c11f118e46b21c9
Seongjin Kang <seongjin4349@gmail.com>
2023-03-07 21:47:08 +0900

    t2

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("어쩌구저쩌구");
+        System.out.println("더 많은 테스트");
     }
 }
